b15c30114f68eb57692cfb0d9313da16a41b1e07
Michaël Zasso <targos@protonmail.com>
2019-06-29 13:58:41 +0200

    chore: update dependencies

--- gaussian-kernel.js
+++ gaussian-kernel.js
@@ -1,22 +1,21 @@
 'use strict';
 
-const squaredEuclidean = require('ml-distance-euclidean').squared;
+const { squaredEuclidean } = require('ml-distance-euclidean');
 
 const defaultOptions = {
-    sigma: 1
+  sigma: 1
 };
 
 class GaussianKernel {
-    constructor(options) {
-        options = Object.assign({}, defaultOptions, options);
-        this.sigma = options.sigma;
-        this.divisor = 2 * options.sigma * options.sigma;
-    }
-
-    compute(x, y) {
-        const distance = squaredEuclidean(x, y);
-        return Math.exp(-distance / this.divisor);
-    }
+  constructor(options) {
+    options = Object.assign({}, defaultOptions, options);
+    this.sigma = options.sigma;
+    this.divisor = 2 * options.sigma * options.sigma;
+  }
+  compute(x, y) {
+    const distance = squaredEuclidean(x, y);
+    return Math.exp(-distance / this.divisor);
+  }
 }
 
 module.exports = GaussianKernel;
